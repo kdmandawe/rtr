09a3acf0555eea2fe17a205419b2fc662e3c79d8
kmandawe <kdmandawe06@yahoo.com.ph>
2018-05-05 13:00:50 +0800

    TDD: Made BasicToyRobot#doPlace unit tests pass

--- src/main/java/com/rea/toyrobot/robot/BasicToyRobot.java
+++ src/main/java/com/rea/toyrobot/robot/BasicToyRobot.java
@@ -2,14 +2,18 @@ package com.rea.toyrobot.robot;
 
 import com.rea.toyrobot.placement.Placement;
 import com.rea.toyrobot.tabletop.TableTop;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BasicToyRobot implements ToyRobot {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BasicToyRobot.class);
 
     private TableTop tableTop;
     private Placement placement;
     private boolean inPlay;
 
-    public BasicToyRobot(TableTop tableTop) {
+    BasicToyRobot(TableTop tableTop) {
         this.tableTop = tableTop;
         this.inPlay = false;
     }
@@ -25,8 +29,15 @@ public class BasicToyRobot implements ToyRobot {
     }
 
     @Override
-    public void doPlace(Placement placement) {
-
+    public void doPlace(Placement newPlacement) {
+        LOG.debug("Executing PLACE Command..");
+        if(actionValid(newPlacement)) {
+            placement = newPlacement;
+            inPlay = true;
+        } else {
+            LOG.warn("Invalid PLACE Command. Ignoring..");
+        }
+        LOG.debug("Current: {}", placement);
     }
 
     @Override
@@ -42,5 +53,16 @@ public class BasicToyRobot implements ToyRobot {
     @Override
     public void doReport() {
 
+    }
+
+    private boolean actionValid(Placement newPlacement) {
+        boolean valid = true;
+        boolean invalidX = newPlacement.getxPosition() < 0 || newPlacement.getxPosition() > tableTop.getMaxXPosition();
+        boolean invalidY = newPlacement.getyPosition() < 0 || newPlacement.getyPosition() > tableTop.getMaxYPosition();
+        boolean robotWillFall =  invalidX || invalidY ;
+        if(!tableTop.allowFall() && robotWillFall) {
+            valid = false;
+        }
+        return valid;
     }
 }
--- src/main/java/com/rea/toyrobot/tabletop/SquareTableTopNoFall.java
+++ src/main/java/com/rea/toyrobot/tabletop/SquareTableTopNoFall.java
@@ -2,11 +2,11 @@ package com.rea.toyrobot.tabletop;
 
 public class SquareTableTopNoFall extends SquareTableTop {
 
-    public SquareTableTopNoFall(int width, int height) {
+    SquareTableTopNoFall(int width, int height) {
         super(width, height);
     }
     @Override
     public boolean allowFall() {
-        return true;
+        return false;
     }
 }
